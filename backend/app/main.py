dbaf2791673c9449138c24a5d8556eaf1ec073d9
leedoming <smn0715@naver.com>
2026-03-05 19:20:29 +0900

    feat: env 환경변수 설정

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173", 
         "http://localhost:5174", 
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "https://d1otafw1wb5gvu.cloudfront.net"
     ] + settings.cors_origins_list,
     allow_credentials=True,
     allow_methods=["*"],
